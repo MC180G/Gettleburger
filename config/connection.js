414aefb6daeb2f2a2507e78f3055d2e3aece2f68
Matt Colson <mc180g@gmail.com>
2020-02-15 21:49:47 -0500

    changed port number

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ var mysql = require("mysql");
 
 var connection = mysql.createConnection({
   host: "localhost",
-  port: 3306,
+  port: 5000,
   user: "root",
   password: "",
   database: "cdtgvbis6iwt3o4e"
