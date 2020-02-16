6a22fc8bff97cbd488c3b131a9c23f4415f0bb83
Matt Colson <mc180g@gmail.com>
2020-02-15 21:21:56 -0500

    added Heroku ReadMe and JAWS_DB

--- config/connection.js
+++ config/connection.js
@@ -8,6 +8,17 @@ var connection = mysql.createConnection({
   database: "burger_db"
 });
 
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'burger_db'
+  });
+};
+
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
